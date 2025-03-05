81cc0e327bc229d3fa14d95539fa527991bbcae8
ИСП-34 <-34@LAB24-02>
2025-03-05 11:40:46 +0300

    Добавьте файлы проекта.

---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Notepad;
+
+namespace Notepad
+{
+    
+    public partial class MainWindow : Window
+    {
+        Notepad notepad;
+        public MainWindow()
+        {
+            InitializeComponent();
+            notepad = new Notepad(fieldEdit);
+        }
+        //Кнопка создания
+        public void Create_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.Create();//Вызов метода создания нового документа
+            this.Title = notepad.NameFile; //Обновляем заголовок окна
+        }
+        //Кнопка открытия
+        public void Open_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.Load();//Вызов метода загрузки текста из файла
+            this.Title = notepad.NameFile; //Обновляем заголовок окна
+        }
+        //Кнопка сохранения
+        public void Save_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.ASaveNotepad();//Вызов метода сохранения текста
+            this.Title = notepad.NameFile; //Обновляем заголовок окна
+        }
+        //Кнопка сохранения как
+        public void ASave_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.SaveAs();//Вызов метода сохранения длкумента как
+            this.Title = notepad.NameFile; //Обновляем заголовок окна
+        }
+        //Кнопка выхода
+        public void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            if (notepad.Exit())
+                this.Close();
+        }
+        //Кнопка копирования
+        public void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.Copy();
+        }
+        //Кнопка вставления
+        public void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.Paste();
+        }
+        //Кнопка отменения
+        public void Undo_Click(object sender, RoutedEventArgs e)
+        {
+            notepad.Undo();
+        }
+        //Кнопка о программе
+        public void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Программа: Разработка блокнота\n\nРазработчик: Сухомяткина Ксения Игоревна\nГруппа: ИСП-34", "О Программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Notepad/Notepad.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32; 
+using System.IO; 
+using System.Security.Policy;
+using System.Windows; 
+using System.Windows.Controls; 
+using System.Windows.Documents; 
+
+namespace Notepad
+{
+    class Notepad
+    {
+        string nameFile; //хранит имя текущего файла
+        RichTextBox fieldEdit; //Поле редактирования текста
+        public bool Modified { get; set; } //флаг, указывающий был ли текст изменен
+        public string NameFile //св-во для получения имени файла
+        {
+            get { return nameFile; }
+        }
+        public Notepad(RichTextBox fieldEdit) //Конструктор класса, инициализирует поле редактирования и сбрасывает флаг изменения
+        {
+            nameFile = ""; //Изначально имя файла пустое
+            this.fieldEdit = fieldEdit; //Присваиваем переданное поле редактирования
+            Modified = false; //Текст еще не изменен
+        }
+        public bool ASaveNotepad() //Метод для сохранения текста в файл
+        {
+            SaveFileDialog sd = new SaveFileDialog(); //Создаем диалог сохранения файда
+            sd.DefaultExt = "rtf"; //Устанавливаем по умолчанию
+            sd.Filter = "Текстовый файл (*.rtf)|*.rtf|Все файлы(*.*)|*.*"; //Фильтр для выбора типа файла
+
+            if (nameFile == "") //Если имя файоа не задано
+            {
+                if (sd.ShowDialog() == true) //открываем диалог сохранения и проверяем, что пользователь выбрал файл
+                    nameFile = sd.FileName; //Сохраняем выбранное имя файла
+                else return false; //Если отмена, возвращем false
+            }
+            //Сохраняем содержимое RichTextBox в файл
+            TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd); //Получаем текст из RichTextBox
+            FileStream fs = File.Create(nameFile); //Создаем файл для записи
+            doc.Save(fs, DataFormats.Rtf); //Сохраняем текст в формате rtf
+            fs.Close(); //Закрываем поток
+            Modified = false; //Сбрасываем изменения
+            return true; //Возвращаем успешный результат
+        }
+        //Метод создания нового документа
+        public void Create()
+        {
+            if (Modified == true) //Если текст был изменен
+            {
+                MessageBoxResult result = MessageBox.Show("Вы хотите сохранить изменения в файле?", "Notepad", MessageBoxButton.YesNoCancel); //Запрашиваем пользоватея о сохранении
+                if (result == MessageBoxResult.Yes) //Если да
+                {
+                    if (!ASaveNotepad()) return; //Пытаемся сохранить файл, если не удалось, то выходим
+                }
+                else if (result == MessageBoxResult.Cancel) //Если отмена
+                {
+                    return; //Прерываем создание новго документа
+                }
+            }
+            fieldEdit.Document.Blocks.Clear(); //Очищаем содержимое RichTextBox
+            nameFile = ""; //Сбрасываем имя файла
+            Modified = false; //Сбрасываем изменения
+        }
+        //Метод для загрузки текста из файла
+        public void Load()
+        {
+            OpenFileDialog od = new OpenFileDialog(); //Создаем диалог открытия файла
+            od.DefaultExt = "rtf"; //Устанавливаем расширение по умолчанию
+            od.Filter = "Текстовый файл (*.rtf)|*.rtf|Все файлы(*.*)|*.*"; //Фильтр для выбора типа файла
+            if (od.ShowDialog() == true) //Открытие диалога, проверка, что пользователь выбрал файл
+            {
+                nameFile = od.FileName; //Сохраняем имя выбранного файла
+                TextRange doc = new TextRange(fieldEdit.Document.ContentStart, fieldEdit.Document.ContentEnd); //Получаем текст из RichTextBox
+                FileStream fs = File.Create(nameFile); //Создаем файл для записи
+                doc.Load(fs, DataFormats.Rtf); //Сохраняем текст в формате TXT
+                fs.Close(); //Закрываем поток
+                Modified = false; //Сбрасываем изменения
+            }
+        }
+        //Метод для сохранения текста с новым именем
+        public void SaveAs()
+        {
+            string oldName = nameFile; //Сохраняем старое имя файла
+            nameFile = ""; //Сбрасываем имя файла для того чтобы вызвать диалог сохранения
+            if (!ASaveNotepad()) //Попытка сохранения файла с новым именем
+            {
+                nameFile = oldName; //Если отмена, восстанавливаем старое имя файла
+            }
+        }
+        //Метод для завершения работы блокнота
+        public bool Exit()
+        {
+            if (Modified) //Если текст был изменен
+            {
+                MessageBoxResult result = MessageBox.Show("Вы хотите сохранить изменения в файле?", "Notepad", MessageBoxButton.YesNoCancel); //Запрашиваем пользоватея о сохранении
+                if (result == MessageBoxResult.Yes) //Если да
+                {
+                    if (!ASaveNotepad()) return false; //Пытаемся сохранить файл, если не удалось, то выходим
+                }
+                else if (result == MessageBoxResult.Cancel) //Если отмена
+                {
+                    return false; //Прерываем выход
+                }
+            }
+            return true; //разрешаем выход
+        }
+        //Метод копирования текст в буфер обмена
+        public void Copy()
+        {
+            fieldEdit.Copy();//Копируем выделенный текст в буфер обмена
+        }
+        //Метод для вставки текста из буфера обмена
+        public void Paste()
+        {
+            fieldEdit.Paste(); //Вставляем текст из буфера обмена
+        }
+        //Метод для отмены последнего действия
+        public void Undo()
+        {
+            fieldEdit.Undo();//Отменяем последнее действие 
+        }
+    }
+}
